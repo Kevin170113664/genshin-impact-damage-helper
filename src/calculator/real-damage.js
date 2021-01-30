b2ff1021ea0c25221402693bc5f916ed975b3ef7
Li Linhao <lhli@thoughtworks.com>
2021-01-30 14:41:21 +0800

    添加等级压制计算公式

--- src/calculator/real-damage.js
+++ src/calculator/real-damage.js
@@ -1,31 +1,31 @@
-// DPS=(r/t)*s*DR*RR*AT*CR*DB
-// 我们可以有7个相乘的伤害区间来调整伤害，r是动作伤害倍率，t为动作持续时间 ，s为攻速 ，DR是防御减免效果，RR是元素抗性，AT为攻击力，CR为暴击收益，DB为增伤。其中除暴击收益CR外，每个区间内的相互加成为加值。
-
-// (L+100)/(l+L+200)
-// 其中l为怪物等级，L为角色等级
 import {round2} from './rounding';
 
 export class Calculator {
-  constructor({attack, criticalRatio, criticalDamage, ratio, level}) {
+  constructor({attack, criticalRatio, criticalDamage, ratio, level}, targetStatistics) {
     this.attack = attack;
     this.criticalRatio = criticalRatio;
     this.criticalDamage = criticalDamage;
     this.ratio = ratio;
     this.level = level;
 
-    this.target = {
-      level: level,
-      resistRatio: 0.1
-    }
+    this.target = targetStatistics
   }
 
   calculate() {
-    const levelMultiplier = (this.level + 100) / (this.level + this.target.level + 200);
+    const attack = this.attack * this.levelMultiplier;
 
-    const normalDamage = this.attack * this.ratio * (1 - this.target.resistRatio) * levelMultiplier;
-    const criticalDamage = this.attack * this.ratio * (1 - this.target.resistRatio) * (1 + this.criticalDamage) * levelMultiplier;
+    const normalDamage = attack * this.ratio * (1 - this.target.resistRatio);
+    const criticalDamage = attack * this.ratio * (1 - this.target.resistRatio) * (1 + this.criticalDamage);
     const damageExpectation = this.criticalRatio * criticalDamage + (1 - this.criticalRatio) * normalDamage;
 
     return [round2(damageExpectation), round2(normalDamage), round2(criticalDamage)]
+  }
+
+  get levelMultiplier() {
+    if (this.target.level - this.level >= 70 && this.level <= 10) {
+      return (this.level + 100) / (this.level + this.target.level + 200) * 0.5
+    }
+
+    return (this.level + 100) / (this.level + this.target.level + 200)
   }
 }
--- src/calculator/real-damage.test.js
+++ src/calculator/real-damage.test.js
@@ -2,18 +2,42 @@ import {Calculator} from './real-damage';
 
 describe('real damage calculator', () => {
   test('should be able to calculate first normal attack damage for 甘雨', () => {
-    const GanyuStatics = {
+    const GanyuStatistics = {
       attack: 100,
       criticalRatio: 0.05,
       criticalDamage: 0.5,
       ratio: 0.317,
       level: 1
     };
+    const targetStatistics = {
+      level: 1,
+      resistRatio: 0.1
+    }
 
-    const [damageExpectation, normalDamage, criticalDamage] = new Calculator(GanyuStatics).calculate();
+    const [damageExpectation, normalDamage, criticalDamage] = new Calculator(GanyuStatistics, targetStatistics).calculate();
 
     expect(damageExpectation).toEqual(14.62);
     expect(normalDamage).toEqual(14.27);
     expect(criticalDamage).toEqual(21.40);
+  })
+
+  test('should be able to calculate first normal attack damage for 甘雨 with target has a high level', () => {
+    const GanyuStatistics = {
+      attack: 100,
+      criticalRatio: 0.05,
+      criticalDamage: 0.5,
+      ratio: 0.317,
+      level: 1
+    };
+    const targetStatistics = {
+      level: 90,
+      resistRatio: 0.1
+    }
+
+    const [damageExpectation, normalDamage, criticalDamage] = new Calculator(GanyuStatistics, targetStatistics).calculate();
+
+    expect(damageExpectation).toEqual(5.07);
+    expect(normalDamage).toEqual(4.95);
+    expect(criticalDamage).toEqual(7.43);
   })
 })
