556cf10448f9530c8e4f2b20b4f7c57e49efa962
Li Linhao <lhli@thoughtworks.com>
2021-01-31 00:44:23 +0800

    添加命座4的增伤

--- src/scenario/ganyu.js
+++ src/scenario/ganyu.js
@@ -21,40 +21,62 @@ export class Scenario {
     const PassiveTalentBonus = this.characterStats.level > 70 ? 0.2 : 0;
     const targetResistanceReduction = this.constellation >= 1 ? 0.15 : 0;
 
-    return [
-      {
-        description: '发动试做澹月特效前',
-        characterStats: {
-          ...this.characterStats,
-          attack: this.basicAttack + this.additionalAttack,
+    const scenario1 = {
+      description: '发动试做澹月特效前',
+      characterStats: {
+        ...this.characterStats,
+        attack: this.basicAttack + this.additionalAttack,
+      }
+    };
+
+    const scenario2 = {
+      description: '发动试做澹月特效后(双冰共鸣，20突破天赋，冰套4)',
+      characterStats: {
+        ...this.characterStats,
+        attack: round0(this.basicAttack * (1 + attackIncreaseRatio) + this.additionalAttack),
+        criticalRatio: round2(this.characterStats.criticalRatio + shatteringIceBonus + undividedHeartBonus + blizzardStrayerBonus),
+      },
+      targetStats: {
+        resistRatio: round2(0.1 - targetResistanceReduction)
+      }
+    };
+
+    const scenario3 = {
+      description: '发动试做澹月特效后站在冰雨内(双冰共鸣，20突破天赋，冰套4)',
+      characterStats: {
+        ...this.characterStats,
+        attack: round0(this.basicAttack * (1 + attackIncreaseRatio) + this.additionalAttack),
+        criticalRatio: round2(this.characterStats.criticalRatio + shatteringIceBonus + undividedHeartBonus + blizzardStrayerBonus),
+        damageBoost: {
+          ...this.characterStats.damageBoost,
+          other: round2((this.characterStats.damageBoost.other || 0) + PassiveTalentBonus)
         }
       },
-      {
-        description: '发动试做澹月特效后(双冰共鸣，20突破天赋，冰套4)',
-        characterStats: {
-          ...this.characterStats,
-          attack: round0(this.basicAttack * (1 + attackIncreaseRatio) + this.additionalAttack),
-          criticalRatio: round2(this.characterStats.criticalRatio + shatteringIceBonus + undividedHeartBonus + blizzardStrayerBonus),
-        },
-        targetStats: {
-          resistRatio: round2(0.1 - targetResistanceReduction)
-        }
-      },
-      {
-        description: '发动试做澹月特效后站在冰雨内(双冰共鸣，20突破天赋，冰套4)',
+      targetStats: {
+        resistRatio: round2(0.1 - targetResistanceReduction)
+      }
+    };
+
+    if (this.constellation >= 4) {
+      const forthConstellationMaxBonus = 0.25;
+      const scenario4 = {
+        description: '发动试做澹月特效后站在冰雨内吃满4命增伤(双冰共鸣，20突破天赋，冰套4)',
         characterStats: {
           ...this.characterStats,
           attack: round0(this.basicAttack * (1 + attackIncreaseRatio) + this.additionalAttack),
           criticalRatio: round2(this.characterStats.criticalRatio + shatteringIceBonus + undividedHeartBonus + blizzardStrayerBonus),
           damageBoost: {
             ...this.characterStats.damageBoost,
-            other: (this.characterStats.damageBoost.other || 0) + PassiveTalentBonus
+            other: round2((this.characterStats.damageBoost.other || 0) + PassiveTalentBonus + forthConstellationMaxBonus)
           }
         },
         targetStats: {
           resistRatio: round2(0.1 - targetResistanceReduction)
         }
       }
-    ]
+      return [scenario1, scenario2, scenario3, scenario4]
+    }
+
+    return [scenario1, scenario2, scenario3]
   }
 }
--- src/scenario/ganyu.test.js
+++ src/scenario/ganyu.test.js
@@ -74,4 +74,95 @@ describe('scenario', () => {
       }
     ])
   })
+
+  test('should be able to generate 甘雨 scenario with prototype crescent and has forth constellation', () => {
+    const ganyuStats = {
+      basicAttack: 823,
+      additionalAttack: 1215,
+      criticalRatio: 0.19,
+      criticalDamage: 2.198,
+      level: 81,
+      mastery: 0,
+      constellation: 4,
+      talentLevels: [11, 3, 6],
+      damageBoost: {
+        [E.CRYO]: 0.616
+      }
+    };
+    const weaponStats = {
+      name: 'prototype_crescent',
+      refineRank: 3
+    };
+    const scenarios = new Scenario(ganyuStats, weaponStats).generate();
+
+    expect(scenarios).toEqual([
+      {
+        description: '发动试做澹月特效前',
+        characterStats: {
+          attack: 2038,
+          criticalRatio: 0.19,
+          criticalDamage: 2.198,
+          level: 81,
+          mastery: 0,
+          talentLevels: [11, 3, 6],
+          damageBoost: {
+            [E.CRYO]: 0.616
+          }
+        }
+      },
+      {
+        description: '发动试做澹月特效后(双冰共鸣，20突破天赋，冰套4)',
+        characterStats: {
+          attack: 2482,
+          criticalRatio: 0.74,
+          criticalDamage: 2.198,
+          level: 81,
+          mastery: 0,
+          talentLevels: [11, 3, 6],
+          damageBoost: {
+            [E.CRYO]: 0.616
+          }
+        },
+        targetStats: {
+          resistRatio: -0.05
+        }
+      },
+      {
+        description: '发动试做澹月特效后站在冰雨内(双冰共鸣，20突破天赋，冰套4)',
+        characterStats: {
+          attack: 2482,
+          criticalRatio: 0.74,
+          criticalDamage: 2.198,
+          level: 81,
+          mastery: 0,
+          talentLevels: [11, 3, 6],
+          damageBoost: {
+            [E.CRYO]: 0.616,
+            other: 0.2
+          }
+        },
+        targetStats: {
+          resistRatio: -0.05
+        }
+      },
+      {
+        description: '发动试做澹月特效后站在冰雨内吃满4命增伤(双冰共鸣，20突破天赋，冰套4)',
+        characterStats: {
+          attack: 2482,
+          criticalRatio: 0.74,
+          criticalDamage: 2.198,
+          level: 81,
+          mastery: 0,
+          talentLevels: [11, 3, 6],
+          damageBoost: {
+            [E.CRYO]: 0.616,
+            other: 0.45
+          }
+        },
+        targetStats: {
+          resistRatio: -0.05
+        }
+      }
+    ])
+  })
 })
